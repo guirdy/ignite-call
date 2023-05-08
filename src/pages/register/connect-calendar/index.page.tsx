9722396deb2e4f21fceeb202d22633082c62e119
guirdy <guilherme.gl1997@gmail.com>
2023-05-08 12:54:10 -0300

    feat: next auth

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -1,10 +1,13 @@
 import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
+import { signIn, useSession } from 'next-auth/react'
 import { ArrowRight } from 'phosphor-react'
 // import { api } from "../../../lib/axios"
 import { Container, Header } from '../styles'
 import { ConnectBox, ConnectItem } from './styles'
 
 export default function Register() {
+  const session = useSession()
+
   // async function handleRegister() {
 
   // }
