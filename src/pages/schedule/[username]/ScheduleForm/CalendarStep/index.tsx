f5501f84e23a484df1a7b050ff743185c5d466a9
guirdy <guilherme.gl1997@gmail.com>
2023-06-26 17:52:32 -0300

    feat: calendar structure visual

---
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -0,0 +1,10 @@
+import { Calendar } from '@/pages/home/components/Calendar'
+import { Container } from './styles'
+
+export function CalendarStep() {
+  return (
+    <Container>
+      <Calendar />
+    </Container>
+  )
+}
